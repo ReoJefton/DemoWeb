8320ac0895cbf6a864d65f77118df0604afd2aec
Reo Jefton <31663206+ReoJefton@users.noreply.github.com>
2022-08-23 16:56:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const func = () => {
   console.log('Test')
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
 }
 func()
