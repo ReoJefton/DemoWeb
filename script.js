40f1ed0bf3d23397d82c503d9b921761d9ec8ef9
Reo Jefton <31663206+ReoJefton@users.noreply.github.com>
2022-08-25 17:26:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,22 +13,25 @@ const displayData = (data) => {
   var mainContainer = document.querySelector('#dataContainer')
   const bpiObj = data.bpi
   console.log(bpiObj)
-  const div = document.createElement('div')
-  const div2 = document.createElement('div')
+
+  //const div2 = document.createElement('div')
   Object.keys(bpiObj).forEach((bp) => {
+    const div = document.createElement('div')
+    //div.classList.add('flex')
     const bpObj = bpiObj[bp]
     console.log(bp, bpObj)
     Object.keys(bpObj).forEach((key) => {
       div.innerHTML += key + ':' + bpObj[key] + '<br>'
     })
     div.innerHTML += '<br>'
+    mainContainer.appendChild(div)
     // const bpArray = Object.keys(bpObj).map((key) => [key, bpObj[key]])
     // bpArray.forEach((arr) => {
     //   div2.innerHTML += arr[0] + ':' + arr[1] + '<br>'
     // })
     // div2.innerHTML += '<br>'
   })
-  mainContainer.appendChild(div)
+
   //mainContainer.appendChild(div2)
 }
 
